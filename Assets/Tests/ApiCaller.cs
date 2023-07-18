586c67194bb6887889589ef4f6a4005cfca28258
dbiles <darron.biles@gmail.com>
2023-07-18 13:29:31 -0400

    sample api call

---
+++ Assets/Tests/ApiCaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class APICaller : MonoBehaviour
+{
+    public string apiUrl = "https://cat-fact.herokuapp.com/facts"; 
+
+    public IEnumerator CallAPI()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(apiUrl))
+        {
+            yield return request.SendWebRequest();
+            Debug.Log("Sweet cat data coming back successful!");
+        }
+    }
+}
---
+++ Assets/Tests/unityTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class APICallerTest
+{
+    [UnityTest]
+    public IEnumerator TestAPICall()
+    {
+        APICaller apiCaller = new GameObject().AddComponent<APICaller>();
+        apiCaller.apiUrl = "https://cat-fact.herokuapp.com/facts";
+
+        yield return apiCaller.CallAPI();
+    }
+}
